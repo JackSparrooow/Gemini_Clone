c2f108ded53ade5fc86223c63e47281dd6d2416e
Jack Sparrooow <anuragmule73@gmail.com>
2024-08-25 13:18:50 +0530

    API Implimented

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../../assets/assets";
 import "./Main.css";
+import { Context } from "../../context/context";
 
 const Main = () => {
+
+  const {onSent, recentPrompt, showResult, loading, resultData, setInput, input} = useContext(Context)
+
+
+
+
+
   return (
     <div className="main">
       <div className="nav">
@@ -11,7 +19,10 @@ const Main = () => {
       </div>
 
       <div className="main-container">
-        <div className="greet">
+
+        {!showResult
+        ?<>
+         <div className="greet">
           <p>
             <span>Hello Anup..</span>
           </p>
@@ -35,14 +46,26 @@ const Main = () => {
             <img src={assets.code_icon} alt="" />
           </div>
         </div>
+ 
+        </>
+        : <div className="result">
+          <div className="result-title">
+            <img src={assets.user_icon} alt="" />
+            <p>{recentPrompt}</p>
+          </div>
+        </div>
+        
+        }
 
+
+       
         <div className="main-bottom">
           <div className="search-box">
-            <input type="text" placeholder="Enter a prompt here" />
+            <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder="Enter a prompt here" />
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
-              <img src={assets.send_icon} alt="" />
+              <img onClick={()=> onSent()} src={assets.send_icon} alt="" />
             </div>
           </div>
           <p className="bottom-info">
